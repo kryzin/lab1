914fab5d1f2269c790e1434224f9545b87ab84e4
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2021-02-22 14:00:51 +0100

    zad1&2&3

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-from math import *
-
-a, b, c = 2, 4, 6
+#zad1
+a = 'jest luty'
+b = 2
 print(a)
 print(b)
-print(c)
 
-dzielenie = b/c
-dzielenie_calkowite = b // c
-print(dzielenie_calkowite)
+#zad2
+print('kalkulator:')
+i = 1
+j = 3
+k = 8
+dodawanie = j+k
+odejmowanie = k-i
+potega = k ** j
+dzielenie = j/i
+dzielenie_calkowite = k // j
+mnozenie = j*k
+print('3+8=%d' %(dodawanie))
+print('8-1=%d' %(odejmowanie))
+print('8 do potegi 3=%d' %(potega))
+print('3/1=%d' %(dzielenie))
+print('calkowite 8/3=%d' %(dzielenie_calkowite))
 
-dodawanie = a+b
-potega = b ** a
-print(dodawanie)
-print(potega)
-
-potega = pow(b,a)
-print(potega)
-
-a += 10
-print(a)
-imie = 'Karol'
-wiek = 99
-print('mam na imie %s i mam %d lat' %(imie,wiek))
-
-z= 6 - 4
-print('wynik dzialania %(z1)d-%(z2)d=%(z3)d' % {'z1':c, 'z2':b, 'z3':z})
+#zad3
+b += 10
+j -= 1
+k *= 2
+b /= j
+j **= b
+k %= j
+print(b)
+print(j)
+print(k)
