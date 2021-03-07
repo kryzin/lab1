ad54ba9fb364e5d6dc77b93e07fa1d20c6696fd8
kryzin <76969276+kryzin@users.noreply.github.com>
2021-03-07 11:28:37 +0100

    zad1

---
+++ lab2.py
@@ -0,0 +1,8 @@
+#Zad1
+lista = ['Spirited Away','Sherlock Holmes','Gra tajemnic','Mamma Mia!','Wilcze Dzieci']
+lista.reverse()
+lista.append('Truman Show')
+lista.append('Bao')
+lista.append('Deadpool')
+lista.append('Charlie')
+lista.append('Shrek 2')
