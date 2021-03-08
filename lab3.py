f13c174cdc877323e35e97703d8f67d46be37e54
kryzin <76969276+kryzin@users.noreply.github.com>
2021-03-08 14:11:15 +0100

    zad2

--- lab3.py
+++ lab3.py
@@ -5,3 +5,16 @@ B = [4**x for x in range(8)]
 print(B)
 C = [x for x in B if x%2 == 0]
 print(C)
+
+#Zad2
+import random
+lista1 = []
+lista2 = []
+for x in range(1,11,1):
+    a = random.randint(-100,100)
+    lista1.append(a)
+    if (a%2 == 0):
+        lista2.append(a)
+
+print(lista1)
+print(lista2)
