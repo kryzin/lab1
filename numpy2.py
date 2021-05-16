9e5546f37e2760329cbd3c0fb08f723ea95c597d
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2021-05-16 20:49:08 +0200

    numpy2 10

--- numpy2.py
+++ numpy2.py
@@ -27,12 +27,12 @@ c4 = a4 * b4
 print(c4)
 
 #5
-m5 = np.array([[12, 13], [0, 3], [9, 10]])
-a5 = np.sin(m5)
+c5 = np.array([[12, 13], [0, 3], [9, 10]])
+a5 = np.sin(c5)
 print(a5)
 
 #6
-b6 = np.cos(m5)
+b6 = np.cos(c5)
 print(b6)
 
 #7
@@ -40,3 +40,25 @@ a7 = np.add(a,b)
 print(a7)
 
 #8
+a8 = np.array([[1, 11, 12], [2, 21, 22], [3, 31, 32]])
+for i in a8:
+    print(i)
+    print(" ")
+
+#9
+for i in a8.flat:
+    print(i)
+    print(" ")
+
+#10
+m = np.arange(0,81,1).reshape(9,9)
+print(m)
+print("możliwości: tabele o 81 elementach")
+m1 = m.reshape(3,27)
+print(m1)
+m2 = m.reshape(27,3)
+print(m2)
+m3 = m.reshape(81,1)
+print(m3)
+m4 = m.ravel()
+print(m4)
